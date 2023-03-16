7e70fe66b2b9e4efe820e0c644ab573d1e7f3e45
Joaquin Diaz <joacod@gmail.com>
2023-03-16 16:03:07 -0300

    lyrics route example

--- src/app.py
+++ src/app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/lyrics/<artist>/<song>')
+def lyrics(artist, song):
+    return 'Lyrics for %s by %s' % (song, artist)
